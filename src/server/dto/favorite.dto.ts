2a268502380057311452b85e14c1e618ad30935b
Sebastien <sebastien.thibaut@gmail.com>
2017-04-14 23:58:36 -0400

    Create separate definitions to Dto

--- src/server/dto/favorite.dto.ts
+++ src/server/dto/favorite.dto.ts
@@ -1,73 +1,7 @@
-import Dto from './dto';
-import {IDto} from './dto';
+import {Link} from '../entities/link';
 
-export interface IFavoriteDto extends IDto {
-  id: string;
+export interface FavoriteDto {
   name: string;
-  createdOn: string;
-  index: string;
   folderPath: string;
-}
-
-export default class FavoriteDto extends Dto implements IFavoriteDto {
-  id: string;
-  name: string;
-  createdOn: string;
-  index: string;
-  folderPath: string;
-
-  static toDto(data, options) {
-    options = FavoriteDto.safeOptions(options);
-
-    FavoriteDto.assertValidData(data, options);
-    return new FavoriteDto(data, options.overrideId);
-  }
-
-  static safeOptions(options) {
-    if (!options) return {};
-    options.overrideId = options.overrideId || null;
-    options.checkAllRequiredFields = options.checkAllRequiredFields || false;
-    options.checkAllRequiredFieldsButId = options.checkAllRequiredFieldsButId || false;
-    return options;
-  }
-
-  static assertValidData(data, options) {
-    if (data === undefined) {
-      throw new Error('No data has been provided for favorite');
-    }
-    if (options.checkAllRequiredFields && !data.id && !options.overrideId) {
-      throw new Error('If data does not contain an Id, you have to use overrideId');
-    }
-    if (options.checkAllRequiredFields && options.overrideId && typeof options.overrideId !== 'string') {
-      throw new Error('Overrode id is not of type String');
-    }
-    if (options.checkAllRequiredFields && !options.overrideId && typeof data.id !== 'string') {
-      throw new Error('Id must be defined and of type String');
-    }
-    if ((data.id && data.id.length > 24) || (options.overrideId && options.overrideId.length > 24)) {
-      throw new Error('Overrode id is not of type String');
-    }
-    if (options.checkAllRequiredFields || options.checkAllRequiredFieldsButId && typeof data.name !== 'string') {
-      throw new Error('Name must be specified and of type String');
-    }
-    if (data.createdOn && !(data.createdOn instanceof Date)) {
-      throw new Error('CreatedOn must be of type Date');
-    }
-    if (data.index && typeof data.index !== 'number') {
-      throw new Error('Specified index must be of type Number');
-    }
-    if (options.checkAllRequiredFields || options.checkAllRequiredFieldsButId && typeof data.folderPath !== 'string') {
-      throw new Error('folderPath must be specified and of type String');
-    }
-  }
-
-  constructor(data, overrideId) {
-    super();
-
-    this.id = overrideId || data.id;
-    if ('name' in data) this.name = data.name;
-    if ('createdOn' in data) this.createdOn = data.createdOn;
-    if ('index' in data) this.index = data.index;
-    if ('folderPath' in data) this.folderPath = data.folderPath;
-  }
+  links: Link[]
 }
--- src/server/dto/user.dto.ts
+++ src/server/dto/user.dto.ts
@@ -1,79 +1,11 @@
-import Dto from './dto';
-import UserPermissionsDto, {IUserPermissionsDto} from './userPermissions.dto';
-import {IDto} from './dto';
+import {UserPermissionsDto} from './userPermissions.dto';
 
-export interface IUserDto extends IDto {
+export interface UserDto {
   id: string;
-  isActive: string;
+  isActive: boolean;
   username: string;
   fullName: string;
   email: string;
   password: string;
-  permissions: IUserPermissionsDto;
-}
-
-export default class UserDto extends Dto implements IUserDto {
-  id: string;
-  isActive: string;
-  username: string;
-  fullName: string;
-  email: string;
-  password: string;
-  permissions: IUserPermissionsDto;
-
-  static safeOptions(options) {
-    return options || {};
-  }
-
-  static assertValidData(data, options) {
-    if (data === undefined) {
-      throw new Error('No data has been provided for user');
-    }
-
-    if (options.checkAllRequiredFields && !data.id && !options.overrideId) {
-      throw new Error('If data does not contain an Id, you have to use overrideId');
-    }
-    if (options.checkAllRequiredFields && options.overrideId && typeof options.overrideId !== 'string') {
-      throw new Error('Overrode id is not of type String');
-    }
-    if (options.checkAllRequiredFields && !options.overrideId && typeof data.id !== 'string') {
-      throw new Error('Id must be defined and of type String');
-    }
-    if ((data.id && data.id.length > 24) || (options.overrideId && options.overrideId.length > 24)) {
-      throw new Error('Overrode id is not of type String');
-    }
-    if (data.isActive && typeof data.isActive !== 'boolean') {
-      throw new Error('isActive must be of type Boolean');
-    }
-    if (data.username && typeof data.username !== 'string') {
-      throw new Error('username must be of type String');
-    }
-    if (data.fullName && typeof data.fullName !== 'string') {
-      throw new Error('username must be of type String');
-    }
-    if (data.email && typeof data.email !== 'string') {
-      throw new Error('email must be of type String');
-    }
-    if (data.password && typeof data.password !== 'string') {
-      throw new Error('password must be of type String');
-    }
-  }
-
-  static toDto(data, options) {
-    options = UserDto.safeOptions(options);
-    UserDto.assertValidData(data, options);
-    return new UserDto(data, options.overrideId);
-  }
-
-  constructor(data, overrideId) {
-    super();
-
-    this.id = overrideId || data.id;
-    if ('isActive' in data) this.isActive = data.isActive;
-    if ('username' in data) this.username = data.username;
-    if ('fullName' in data) this.fullName = data.fullName;
-    if ('email' in data) this.email = data.email;
-    if ('password' in data) this.password = data.password;
-    this.permissions = new UserPermissionsDto(data);
-  }
+  permissions: UserPermissionsDto;
 }
