0168b5b2d3487e6286a9eb496e806fb722070708
Sebastien <sebastien.thibaut@gmail.com>
2017-05-07 15:13:17 -0400

    Move mediators

---
+++ client/src/mediators.ts
@@ -0,0 +1,13 @@
+/// This file is temporary and will be removed soon
+
+export default class Mediators {
+  isUserStateInitialized = false;
+
+  getIsUserStateInitialized(): boolean {
+    return this.isUserStateInitialized;
+  }
+
+  setIsUserStateInitialized(value: boolean): void {
+    this.isUserStateInitialized = value;
+  }
+}
